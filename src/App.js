dd20acec79325d0dfe7ee53409cb8f3efea4d0c9
Destiny Rogers <destinyrogers725@gmail.com>
2020-01-30 23:15:23 -0800

    i did it

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Quiz from './quiz';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*
+*
+*
+* @author: Destiny Rogers
+* @date: December 15, 2019
+* */
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header"><img className="App-img" src="http://www.fasttimes.biz/8Bit%20Band.png"/>
+        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Yellowtail" />
+        </header>
+        <h1 className="gameName">Music Exploration</h1>
+        <Quiz/>
+      </div>
+    );
+  }
 }
 
 export default App;
+//Answer 6 Questions and I'll Recommend You a Song.
